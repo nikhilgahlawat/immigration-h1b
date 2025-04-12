240cd6fab0a625ef734c4ac7392acbc67fe73b40
Nikhil Gahlawat <nikhil.gahlawat@gmail.com>
2025-04-12 13:58:49 -0400

    add outsourcing company chart

--- src/analysis/charts.R
+++ src/analysis/charts.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 library(scales)
+library(forcats)
 library(showtext)
 # font_add_google("Roboto", "Roboto")
 # showtext_auto()
@@ -9,14 +10,42 @@ library(showtext)
 # Directories and file names
 data_dir <- '../data/analysis/'
 denials_file <- 'denials.csv'
+company_year_file <- 'company_year.csv'
+charts_dir <- '../assets/charts/'
 
 # Load data
 denials <- read.csv(paste0(data_dir, denials_file))
+company_year <- read.csv(paste0(data_dir, company_year_file))
 
-# Format field names
-names(denials) <- gsub('\\.', '_', tolower(names(denials)))
+# Format dataframe field names
+format_field_names <- function(df) {
+  new_names <- gsub('\\.', '_', tolower(names(df)))
+  return(new_names)
+}
+names(denials) <- format_field_names(denials)
+names(company_year) <- format_field_names(company_year)
+
+# Create custom themes
+line_chart_theme <- theme(
+  , text = element_text(family = 'Arial')
+  , panel.background = element_blank()
+  , panel.grid.minor.y = element_blank()
+  , panel.grid.major.x = element_blank()
+  , panel.grid.minor.x = element_blank()
+  , panel.grid.major.y = element_line(color = 'gray90', size = .3)
+  , axis.ticks.y = element_blank()
+  , axis.ticks.x = element_line(size = .4, color = 'black')
+  , axis.ticks.length = unit(0.2, "cm")
+  , axis.line.x = element_blank()
+  , legend.title = element_blank()
+  , plot.title = element_text(face = 'bold', size = 12)
+  , plot.subtitle = element_text(face = 'italic', size = 10, margin = margin(b = 20))
+  , plot.caption = element_text(color = 'gray30', hjust = 0)
+  , legend.key.width = unit(1, "cm")
+  )
 
 # Chart: Denial rates by industry
+## Format data
 plotdata_denials <- denials %>% 
   select(
     fiscal_year
@@ -32,9 +61,17 @@ plotdata_denials <- denials %>%
                         , labels = c('Other', 'Outsourcing', 'Big Tech')
     )
   )
-plotdata_denials
 
+## Create chart
 ggplot(plotdata_denials) +
+  labs(
+    title = 'H-1B Visa Denial Rates'
+    , subtitle = 'Share of applications rejected by the USCIS'
+    , caption = 'Source: USCIS. Industry classification described in methodology.'
+    , x = ''
+    , y = ''
+  ) +
+  line_chart_theme +
   geom_line(
     aes(fiscal_year, denial_rate, color = industry)
     , size = 1
@@ -66,33 +103,11 @@ ggplot(plotdata_denials) +
       , 'Other' = 'gray80'
     )
     , breaks = c('Outsourcing', 'Big Tech', 'Other')
-  ) +
-  theme(
-    , text = element_text(family = 'Arial')
-    , panel.background = element_blank()
-    , panel.grid.minor.y = element_blank()
-    , panel.grid.major.x = element_blank()
-    , panel.grid.minor.x = element_blank()
-    , panel.grid.major.y = element_line(color = 'gray90', size = .3)
-    , axis.ticks.y = element_blank() #element_line(color = 'gray90')
-    , axis.ticks.x = element_line(size = .4, color = 'black')
-    , axis.ticks.length = unit(0.2, "cm")
-    , axis.line.x = element_blank()
-    , legend.title = element_blank()
-    , plot.title = element_text(face = 'bold', size = 12)
-    , plot.subtitle = element_text(face = 'italic', size = 10, margin = margin(b = 20))
-    , plot.caption = element_text(color = 'gray30', hjust = 0)
-  ) +
-  labs(
-    title = 'H-1B Visa Denial Rates'
-    , subtitle = 'Share of applications rejected by the USCIS'
-    , caption = 'Source: USCIS. Industry classification described in methodology.'
-    , x = ''
-    , y = ''
   )
 
 
 # Chart: Denial rates for outsourcing companies
+## Format data
 plotdata_denials_outsourcing <- denials %>% 
   select(
     fiscal_year
@@ -107,9 +122,17 @@ plotdata_denials_outsourcing <- denials %>%
                          , labels = c('New Visas', 'Renewals')
     )
   )
-plotdata_denials_outsourcing
 
+## Create chart
 ggplot(plotdata_denials_outsourcing) +
+  labs(
+    title = 'H-1B Visa Denial Rates for Outsourcing Companies'
+    , subtitle = 'Share of applications rejected by the USCIS'
+    , caption = 'Source: USCIS. Industry classification described in methodology.'
+    , x = ''
+    , y = ''
+  ) +
+  line_chart_theme +
   geom_line(
     aes(fiscal_year, denial_rate, color = visa_type, linetype = visa_type)
     , size = 1
@@ -145,35 +168,99 @@ ggplot(plotdata_denials_outsourcing) +
     values = c(
       "New Visas" = "solid"
       , "Renewals" = "dashed"
-    )) +
-  theme(
-    , text = element_text(family = 'Arial')
-    , panel.background = element_blank()
-    , panel.grid.minor.y = element_blank()
-    , panel.grid.major.x = element_blank()
-    , panel.grid.minor.x = element_blank()
-    , panel.grid.major.y = element_line(color = 'gray90', size = .3)
-    , axis.ticks.y = element_blank() #element_line(color = 'gray90')
-    , axis.ticks.x = element_line(size = .4, color = 'black')
-    , axis.ticks.length = unit(0.2, "cm")
-    , axis.line.x = element_blank()
-    , legend.title = element_blank()
-    , plot.title = element_text(face = 'bold', size = 12)
-    , plot.subtitle = element_text(face = 'italic', size = 10, margin = margin(b = 20))
-    , plot.caption = element_text(color = 'gray30', hjust = 0)
-    , legend.key.width = unit(1, "cm")
-  ) +
-  labs(
-    title = 'H-1B Visa Denial Rates for Outsourcing Companies'
-    , subtitle = 'Share of applications rejected by the USCIS'
-    , caption = 'Source: USCIS. Industry classification described in methodology.'
-    , x = ''
-    , y = ''
-  )
-
+    ))
 
 # svglite::svglite("/Users/nikhilgahlawat/Desktop/test_chart_svglite.svg", width = 6, height = 3.3)
 # print(test_plot)
 # dev.off()
 
+# Chart: Outsourcing company approvals
+## List of companies to display
+outsourcing_company_list <- c(
+  'Cognizant Technology Solutions'
+  , 'Infosys'
+  , 'Tata Consultancy Services'
+  , 'Wipro'
+  , 'Accenture'
+  , 'IBM'
+)
 
+## Format chart data
+plotdata_approvals_outsourcing <- company_year %>% 
+  filter(employer %in% outsourcing_company_list) %>% 
+  select(employer, fiscal_year, initial_approval, continuing_approval, total_approval, share_of_cap) %>% 
+  # Create a field that ranks companies by total approvals
+  # Will use later to determine how companies are ordered in the chart
+  left_join(
+    company_year %>% 
+      filter(employer %in% outsourcing_company_list) %>% 
+      group_by(employer) %>% 
+      summarise(total_approval = sum(total_approval)) %>% 
+      mutate(approvals_rank = rank(total_approval)) %>% 
+      select(-total_approval)
+  ) %>% 
+  mutate(
+    employer = case_when(
+      employer == 'Cognizant Technology Solutions' ~ 'Cognizant'
+      , employer == 'Tata Consultancy Services' ~ 'Tata'
+      , TRUE ~ employer
+      )
+    , employer = fct_reorder(employer, approvals_rank, .desc = TRUE)
+  )
+
+
+## Create chart
+ggplot(plotdata_approvals_outsourcing) +
+  labs(
+    title = 'H-1B Visa Approvals: Outsourcing Companies'
+    , subtitle = 'New visas and renewals approved by the USCIS'
+    , caption = 'Source: USCIS. Industry classification described in methodology.'
+    , x = ''
+    , y = ''
+  ) +
+  line_chart_theme +
+  geom_line(
+    aes(fiscal_year, total_approval, color = employer, linetype = employer)
+    , size = 1
+  ) +
+  geom_segment(
+    aes(x = min(fiscal_year), xend = max(fiscal_year), y = 0, yend = 0)
+    , inherit.aes = FALSE
+    , color = "black"
+    , size = .4
+  ) +
+  scale_y_continuous(
+    # labels = label_number(scale_cut = cut_short_scale())
+    labels = label_comma()
+    , expand = c(0, 0)
+    , limits = c(0, 30e3)
+    , breaks = seq(10e3, 30e3, by = 10e3)
+  ) +
+  scale_x_continuous(
+    breaks = seq(
+      min(plotdata_approvals_outsourcing$fiscal_year)
+      , max(plotdata_approvals_outsourcing$fiscal_year)
+      , by = 2
+    )
+    , expand = c(0,0)
+  ) +
+  scale_color_manual(
+    values = c(
+      'Cognizant' = '#F8AE54'
+      , 'Infosys' = '#CA6C0F'
+      , 'Tata' = '#732E00'
+      , 'Wipro' = '#F8AE54'
+      , 'Accenture' = '#CA6C0F'
+      , 'IBM' = '#732E00'
+    )
+  ) +
+  scale_linetype_manual(
+    values = c(
+      'Cognizant' = 'solid'
+      , 'Infosys' = 'solid'
+      , 'Tata' = 'solid'
+      , 'Wipro' = 'dashed'
+      , 'Accenture' = 'dashed'
+      , 'IBM' = 'dashed'
+      )
+    )
